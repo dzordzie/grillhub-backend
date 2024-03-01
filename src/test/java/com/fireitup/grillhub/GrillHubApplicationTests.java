e4f90a17152613e5322e922c9ea704bec7537d3f
Juraj Valášek <dzordzie0@gmail.com>
2024-03-01 14:43:40 +0100

    initial setup

---
+++ src/test/java/com/fireitup/grillhub/GrillHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fireitup.grillhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrillHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
