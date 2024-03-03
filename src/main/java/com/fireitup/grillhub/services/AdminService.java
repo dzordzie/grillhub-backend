37583c6da1d4a1e291c97a1116498c1641f89d55
Juraj Valášek <dzordzie0@gmail.com>
2024-03-03 12:51:22 +0100

    create AdminService

---
+++ src/main/java/com/fireitup/grillhub/services/AdminService.java
@@ -0,0 +1,39 @@
+package com.fireitup.grillhub.services;
+
+import com.fireitup.grillhub.entities.User;
+import com.fireitup.grillhub.enums.Role;
+import com.fireitup.grillhub.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Value("${admin.email}")
+  private String adminEmail;
+
+  @Value("${admin.name}")
+  private String adminName;
+
+  @Value("${admin.password}")
+  private String adminPassword;
+
+  public void initialAdmin() {
+    if (!userRepository.findByEmail(adminEmail).isPresent()) {
+      User admin = User.builder()
+          .username(adminName)
+          .email(adminEmail)
+          .password(passwordEncoder.encode(adminPassword))
+          .role(Role.ADMIN)
+          .build();
+      userRepository.save(admin);
+    }
+  }
+
+}
