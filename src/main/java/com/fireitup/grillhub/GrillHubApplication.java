3f26910ea428ef63c503f94109ae4f75ee561121
Juraj Valášek <dzordzie0@gmail.com>
2024-03-07 13:07:57 +0100

    remove Exception

--- src/main/java/com/fireitup/grillhub/GrillHubApplication.java
+++ src/main/java/com/fireitup/grillhub/GrillHubApplication.java
@@ -17,7 +17,7 @@ public class GrillHubApplication implements CommandLineRunner {
   }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
     adminService.initialAdmin();
   }
 }
