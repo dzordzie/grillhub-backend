ce35711a1232d7bd2fcf3403098831bba5bbbf3d
Juraj Valášek <dzordzie0@gmail.com>
2024-04-23 16:06:17 +0200

    create method findRubById

--- src/main/java/com/fireitup/grillhub/repositories/RubRepository.java
+++ src/main/java/com/fireitup/grillhub/repositories/RubRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RubRepository extends JpaRepository<Rub, Long> {
   @Query(value = "SELECT * FROM rub", nativeQuery = true)
   List<Rub> findAllRubs();
 
-  Rub findByName(String name);
+  Optional<Rub> findRubById(Long id);
 }
