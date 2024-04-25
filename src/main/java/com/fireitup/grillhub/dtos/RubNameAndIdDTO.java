d05c134a1716ec41917e6c72b89ef8278331a130
Juraj Valášek <dzordzie0@gmail.com>
2024-04-25 12:37:36 +0200

    create new dtos

--- src/main/java/com/fireitup/grillhub/dtos/RubNameAndIdDTO.java
+++ src/main/java/com/fireitup/grillhub/dtos/RubNameAndIdDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class MealNameAndIdDTO {
+public class RubNameAndIdDTO {
   private Long id;
   private String name;
 }
